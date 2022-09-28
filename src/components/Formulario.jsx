6b446b55436e338f653df6ef3ea805f9d81ea9c9
Edith Romero <edithjohanaromero@gmail.com>
2022-09-28 16:48:15 +0200

    Se crea el Context Provider

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+
 const Formulario = () => {
+
+    const [busqueda, setBusqueda] = useState({
+        artista: '',
+        cancion: '',
+    })
+
     return (
         <form>
             <legend>Busca por Artistas y Canción </legend>
@@ -10,6 +18,24 @@ const Formulario = () => {
                         type="text"
                         name="artista"
                         placeholder="Nombre de Artista"
+                        value={busqueda.artista}
+                        onChange={e => setBusqueda({
+                            ...busqueda,
+                            [e.target.name]: e.target.value
+                        })}
+                    />
+                </div>
+                <div>
+                    <label>Artista</label>
+                    <input
+                        type="text"
+                        name="cancion"
+                        placeholder="Nombre de Canción"
+                        value={busqueda.cancion}
+                        onChange={e => setBusqueda({
+                            ...busqueda,
+                            [e.target.name]: e.target.value
+                        })}
                     />
                 </div>
             </div>
